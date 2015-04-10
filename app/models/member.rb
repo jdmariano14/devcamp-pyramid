e3f7950648e26820657f29d020f9e1e8eed26c74
dv_js14 <azyriel14@gmail.com>
2015-04-11 04:24:52 +0800

    Pass previous failing test.

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,5 @@ class Member < ActiveRecord::Base
   NAME_MAX_LENGTH = 60
 
   validates :name, presence: true, length: { maximum: NAME_MAX_LENGTH }
+  validates :money, numericality: { greater_than_or_equal_to: 0 }
 end
