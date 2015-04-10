c00a774e8e85c8d6771896ec1fc25a260fe27a18
dv_js14 <azyriel14@gmail.com>
2015-04-11 04:51:57 +0800

    Pass previous failing test. (for retrieving a member's recruiter)

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,6 @@ class Member < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: NAME_MAX_LENGTH }
   validates :money, numericality: { greater_than_or_equal_to: 0 }
+  
+  belongs_to :recruiter, class_name: "Member"
 end
