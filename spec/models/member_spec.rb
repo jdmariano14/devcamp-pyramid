9b15ed01cbf57ccfc1e21e44a39c28edc44a846d
dv_js14 <azyriel14@gmail.com>
2015-04-11 04:14:49 +0800

    Write failing test for validating presence of a member's name.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
+  before { @member = FactoryGirl.create(:member) }
 
+  subject { @member }
 
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to(:name) }
+  it { should respond_to(:age) }
+  it { should respond_to(:company) }
+  it { should respond_to(:money) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+  	before { @member.name = "     " }
+  	it { should_not be_valid }
+  end
+
 end
