ab071ae8c749fa42d116e8a87ee404dd91f08f69
dv_js14 <azyriel14@gmail.com>
2015-04-11 04:53:19 +0800

    Pass previous failing test.

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,7 @@ class Member < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: NAME_MAX_LENGTH }
   validates :money, numericality: { greater_than_or_equal_to: 0 }
-  
+
   belongs_to :recruiter, class_name: "Member"
+  has_many :recruits, class_name: "Member", foreign_key: "recruiter_id"
 end
