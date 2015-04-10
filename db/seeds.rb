e08c2d3354381b0006963701f57811e594925303
dv_js14 <azyriel14@gmail.com>
2015-04-11 05:59:54 +0800

    Show a member's recruits in their show page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+head = FactoryGirl.create(:member)
+
+3.times do
+  Member.create(name: "FLR_#{('a'..'z').to_a.shuffle[0..5].join('')}", age: 20, company: "Peer MD", money: 10000, recruiter_id: 1)
+end
+
+head.recruits.each do |recruit|
+  2.times do
+    Member.create(name: "SLR_#{('a'..'z').to_a.shuffle[0..5].join('')}", age: 19, company: "Peer MD", money: 100, recruiter_id: recruit.id)	
+  end
+end
